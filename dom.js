94b165b00cea33cc0d6ed9b5066ca81f0008c577
Ky Trieu <hoangky271106@gmail.com>
2026-07-04 23:22:49 +0700

    cleaned css file

--- dom.js
+++ dom.js
@@ -3,23 +3,76 @@ import { getWeatherData } from "./script.js";
 const searchBar = document.getElementById("search-bar");
 
 searchBar.addEventListener('keydown', () => {
+
     if (event.key === 'Enter') {
         event.preventDefault();
         let weatherData = getWeatherData(searchBar.value);
         weatherData.then((data) => {setInformation(data)});
     }
+
 });
 
 const placeName = document.getElementById("place-name");
 const temperature = document.getElementById("temperature");
+const feelsLike = document.getElementById("feels-like");
+const humidity = document.getElementById("humidity");
 const description = document.getElementById("description");
 
 function setInformation(weatherObject) {
+
     let currentPlace = weatherObject.resolvedAddress;
     let currentTemp = weatherObject.currentConditions.temp;
+    let currentFeel = weatherObject.currentConditions.feelslike;
+    let currentHumidity = weatherObject.currentConditions.humidity;
     let currentDescription = weatherObject.description;
 
     placeName.innerHTML = currentPlace;
-    temperature.innerHTML = currentTemp + " Fahrenheit";
+    temperature.innerHTML = "Temperature: " + currentTemp + " Fahrenheit";
+    feelsLike.innerHTML = "Feels like: " + currentFeel + " Fahrenheit";
+    humidity.innerHTML = "Humidity: " + currentHumidity;
     description.innerHTML = currentDescription;
+
 }
+
+const convertButton = document.getElementById("convert");
+let fahrenheit = true;
+convertButton.addEventListener('click', () => {
+
+    let temp = temperature.innerHTML.split(" ")[1];
+    let feels = feelsLike.innerHTML.split(" ")[2];
+
+    if (fahrenheit) {
+
+        let celsiusTemp = (temp - 32) * (5/9);
+        let feelsLikeTemp = (feels - 32) * (5/9);
+
+        celsiusTemp = celsiusTemp.toFixed(1);
+        feelsLikeTemp = feelsLikeTemp.toFixed(1);
+
+        temperature.innerHTML = "Temperature: " + celsiusTemp + " Celsius";
+        feelsLike.innerHTML = "Feels like: " + feelsLikeTemp + " Celsius";
+
+        fahrenheit = false;
+
+    } else {
+
+        let fahrenheitTemp = temp * (9/5) + 32;
+        let feelsLikeTemp = feels * (9/5) + 32;
+
+        fahrenheitTemp = fahrenheitTemp.toFixed(1);
+        feelsLikeTemp = feelsLikeTemp.toFixed(1);
+
+        temperature.innerHTML = "Temperature: " + fahrenheitTemp + " Fahrenheit";
+        feelsLike.innerHTML = "Feels like: " + feelsLikeTemp + " Fahrenheit";
+
+        fahrenheit = true;
+
+    }
+});
+
+const savedPlacesBtn = document.getElementById("saved-places");
+const sidebar = document.getElementById("sidebar");
+
+savedPlacesBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed");
+});
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // ts free
 const API_KEY = "G48JXZYNS8DZKCY7TBXZKR2EB";
+const API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const API_PARAMS = "?key=G48JXZYNS8DZKCY7TBXZKR2EB&&include=hours,current,alerts,fcst";
 
 async function getWeatherData(location) {
 	try {
 		let rawData = await 
-		fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?key=" + API_KEY);
+		fetch(API_URL + location + API_PARAMS);
 		let data = await rawData.json();
+		console.log(data);
 		return data;
 	} catch (error) {
 		throw new Error("Something went wrong");
